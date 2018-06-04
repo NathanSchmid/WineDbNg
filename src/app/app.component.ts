eabc0641af3440b58be2f660ed1c5b9211ddf728
Nathan Schmid <nathan.schmid@gmx.net>
2018-06-04 22:09:01 +0200

    initial import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { Producer } from './shared/producer';
 
 @Component({
   selector: 'winedb-root',
-  templateUrl: './app.component.html',
-  styles: []
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'winedb';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ProducerStoreService } from './shared/producer-store.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProducerListComponent } from './producer-list/producer-list.component';
+import { WineStoreService } from './shared/wine-store.service';
+import { WineListComponent } from './wine-list/wine-list.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { WineDetailsPageComponent } from './wine-details-page/wine-details-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProducerListComponent,
+    WineListComponent,
+    SearchPageComponent,
+    WineDetailsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProducerStoreService, WineStoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
