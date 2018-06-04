072dab9b02e4efadb9b4b05ac9ed74e755862e70
Nathan Schmid <nathan.schmid@gmx.net>
2018-06-04 12:53:48 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'winedb-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'winedb';
+}
