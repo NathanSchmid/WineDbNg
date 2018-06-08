1f7b232a5c84d941e89625e2cfed72c5d7d0ef22
Nathan Schmid <nathan.schmid@gmx.net>
2018-06-08 13:17:21 +0200

    prod endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://localhost:51490/api',
-  labelsEndpoint: 'https://wl59www255.webland.ch/WineLabelHandler.ashx'
+  apiEndpoint: '/app/api',
+  labelsEndpoint: '/WineLabelHandler.ashx'
 };
