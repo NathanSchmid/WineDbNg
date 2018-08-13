e970d36c75d957f9d09158c96f0abcdb6b11980e
Nathan Schmid <nathan.schmid@gmx.net>
2018-08-13 09:40:00 +0200

    Rating should set wine to consumed.

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -11,8 +11,10 @@ import { StateStoreService } from '../shared/state-store.service';
   templateUrl: './checkout-page.component.html',
 })
 export class CheckoutPageComponent implements OnInit {
+  CONSUMED_STATE = 'konsumiert';
   ratings: number[] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
   states: State[];
+  consumedState: State;
   code: string;
   model = {
     rating: undefined,
@@ -28,12 +30,23 @@ export class CheckoutPageComponent implements OnInit {
 
   ngOnInit() {
     this.code = this.route.snapshot.params['code'];
-    this.stateStore.getAll().subscribe(states => this.states = states);
+    this.stateStore.getAll().subscribe(states => {
+      this.states = states;
+      this.consumedState = states.find(state => state.name === this.CONSUMED_STATE);
+    });
   }
 
   onSubmit() {
     this.wineStore.checkout(this.code, this.model.rating, this.model.stateId, this.model.checkoutDate)
       .subscribe(_ => this.location.back());
+  }
+
+  onRate(rating) {
+    // if rated set to consumed too.
+    if (rating && this.consumedState) {
+      this.model.stateId = this.consumedState.id;
+    }
+    this.model.rating = rating;
   }
 
 }
